fd9ca6ac4721614172ef6619a04a77cadf665576
fadhilrobbani <rabbanifadhillailham@gmail.com>
2022-12-07 15:03:56 +0700

    add eslint linter

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 function App() {
   return (
     <div>
-      <p className='bg-red-500 text-slate-50'>kodok dingin</p>
+      <p className="bg-red-500 text-slate-50">kodok dingin</p>
     </div>
   );
 }
