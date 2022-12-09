9e9966ef6ff41eefb88ed632a51480967034ba0d
fadhilrobbani <rabbanifadhillailham@gmail.com>
2022-12-09 16:51:15 +0700

    add register page

--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import RegisterInput from '../components/RegisterInput';
 
 function RegisterPage() {
-  return <div>RegisterPage</div>;
+  const navigate = useNavigate();
+  const onRegisterHandler = () => {
+    navigate('/login');
+  };
+  return (
+    <div className=" text-slate-200 h-screen mt-[-74px] flex justify-center items-center">
+      <RegisterInput onRegister={onRegisterHandler} />
+    </div>
+  );
 }
 
 export default RegisterPage;
